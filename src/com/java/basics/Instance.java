6a150dedd95ae97499f3ef18018893306cf7d9ef
mohan <mohanmarkande5@gmail.com>
2022-12-29 13:18:57 +0530

    My first commit on javabasics

---
+++ src/com/java/basics/Instance.java
@@ -0,0 +1,20 @@
+package com.java.basics;
+
+public class Instance {
+	
+	int age=20;
+	String name="mohan";
+	static int hall=234;
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(hall);
+		System.out.println(new Instance().age);
+		
+		
+	   }
+		
+	}
+
+
---
+++ src/com/java/basics/Parameters.java
@@ -0,0 +1,19 @@
+package com.java.basics;
+
+public class Parameters {
+	
+
+	public static void main(String[] args) {
+		Parameters pm=new Parameters();
+		student(80);
+			
+		
+	}
+	public static void student(int height) {
+		System.out.println(height);
+		
+		
+	}
+	
+
+}
